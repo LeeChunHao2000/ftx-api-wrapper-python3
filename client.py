de91344c7dbc4783750d1706748e591015ac1d03
Lee Chun Hao <meowinhere@gmail.com>
2020-09-14 18:51:55 +0800

    Update client.py

--- client.py
+++ client.py
@@ -112,6 +112,7 @@ class Client(object):
         """
         https://docs.ftx.com/#get-positions
 
+        :param showAvgPrice: display AvgPrice or not
         :return: a dict contains all positions
         """
         
@@ -125,11 +126,12 @@ class Client(object):
         """
 
         return self._send_request('private', 'GET', f"subaccounts")
-
+    
     def get_private_subaccount_balances(self, name):
         """
         https://docs.ftx.com/#get-subaccount-balances
 
+        :param name: the subaccount name to query
         :return: a list contains subaccount balances
         """
 
@@ -143,7 +145,7 @@ class Client(object):
         """
 
         return self._send_request('private', 'GET', f"wallet/coins")
-
+    
     def get_private_wallet_balances(self):
         """
         https://docs.ftx.com/#get-balances
@@ -152,7 +154,7 @@ class Client(object):
         """
 
         return self._send_request('private', 'GET', f"wallet/balances")
-
+    
     def get_private_wallet_all_balances(self):
         """
         https://docs.ftx.com/#get-balances-of-all-accounts
@@ -161,13 +163,40 @@ class Client(object):
         """
         
         return self._send_request('private', 'GET', f"wallet/all_balances")
-
-    # TODO: There is no params to choose deposit blockchain
-    def get_private_wallet_deposit_address(self, coin):
+    
+    def get_private_wallet_deposit_address(self, coin, chain):
         """
         https://docs.ftx.com/#get-deposit-address
 
+        :param currency: the specific coin to endpoint
+        :param chain: the blockchain deposit from
         :return: a list contains deposit address
         """
 
-        return self._send_request('private', 'GET', f"wallet/deposit_address/{coin.upper()}")
+        return self._send_request('private', 'GET', f"wallet/deposit_address/{coin.upper()}", {'method': chain})
+
+    def get_private_wallet_deposit_history(self, limit=20, start_time=None, end_time=None):
+        """
+        https://docs.ftx.com/#get-deposit-history
+
+        :param limit: the records limit to query
+        :param start_time: the target period after Epoch time in seconds
+        :param end_time: the target period before Epoch time in seconds
+        :return: a list contains deposit history
+        """
+
+        query = {
+            'limit': limit,
+        }
+
+        if start_time != None:
+            query.update({ 
+                'start_time': start_time,
+            })
+        
+        if end_time != None:
+            query.update({ 
+                'end_time': end_time
+            })
+
+        return self._send_request('private', 'GET', f"wallet/deposits", query)
